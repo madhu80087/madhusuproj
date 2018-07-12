32b25a3f1fc46f7c4d6400e29b9c0dc71f6c8046
madhu <madhusudhanagolla@gmail.com>
2018-07-12 11:47:41 +0530

    Initial commit

---
+++ madhusuapp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MadhusuappConfig(AppConfig):
+    name = 'madhusuapp'
---
+++ madhusuapp/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import  HttpResponse
+def madhu(requst):
+    return HttpResponse("<html><body bgcolor=green><h1>HELLO MADHU WELCOME TO PYTHON</h1></body></html>")
+
+# Create your views here.
